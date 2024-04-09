2e842c6d10a508c8d76083bd526a9035eae313d5
stepakuryaev <stepakuryaev007@gmail.com>
2024-04-09 23:12:09 +0300

    .

---
+++ 14.py
@@ -0,0 +1,44 @@
+# a = 4**14 + 2**32 - 4
+# print(a)
+# b = bin(a)[2:]
+# print(b.count("1"))
+
+#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# def f(N, d):
+#     res = []
+#     while N != 0:
+#         #print(N)
+#         res.append(N % d)
+#         #print(N % d)
+#         N //= d
+#     return res[::-1]
+#
+
+
+
+
+
+
+# #print(f(21, 6))
+#
+# a = 49**7 + 7**20 - 28
+# print(a)
+#
+# a7 = f(a, 7)
+# print(a7)
+# print(a7.count(0))
+
+
+
+
+# al = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+# for x in al[:16]:
+#     s1 = int('2' + x + "84", 19) #2x84 - 2a84, 2984
+#     s2 = int('2B3' + x, 16)
+#     res = s1 + s2
+#     if res % 88 == 0:
+#         print(x, res//88)
+
+
+
+
